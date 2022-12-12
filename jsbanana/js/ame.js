462c2d1236a608f53ca996b80cfe81819a8f7bd7
kimhyemin9988 <110611596+kimhyemin9988@users.noreply.github.com>
2022-12-12 16:23:45 +0900

    Update ame.js

--- jsbanana/js/ame.js
+++ jsbanana/js/ame.js
@@ -10,20 +10,19 @@ function firstFuntion(event)
 {   event.preventDefault();
     const username = loginBoxJ.value;
     //loginBoxJ에 input된것이 username임
-    console.log(username);
-    localStorage.setItem(USERNAME_KEY,"hyemin");
+    localStorage.setItem(USERNAME_KEY,savedUsername);
     loginFormJ.classList.add(HIDDEN_KEY);
     paintGreetings(savedUsername);
 }
 
-function paintGreetings(username){
-    NaTa.innerHTML ="성공" + username;
+function paintGreetings(savedUsername){
+    NaTa.innerHTML ="성공" + savedUsername;
     NaTa.classList.remove(HIDDEN_KEY);
 }
 //form자체를 가져옴!
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-//저장된 username을 저장하는 변수
+//저장된 username을 저장하는 변수 //localStorage에 key값 "username"으로 저장된 이름(value)
 
 if(savedUsername === null){
     loginFormJ.classList.remove(HIDDEN_KEY);
